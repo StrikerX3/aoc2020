9a6fffa234f19dfe4aa9a28ad829ebec93f0e901
StrikerX3 <ivan.rober@gmail.com>
2020-12-01 09:08:55 -0300

    Day 1

---
+++ adventofcode2020/day1.cpp
@@ -0,0 +1,56 @@
+#include <unordered_set>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+// Part 1 - Two sum
+void part1(std::vector<int>& nums) {
+    std::unordered_set<int> complements;
+    for (int num : nums) {
+        int complement = 2020 - num;
+        if (complements.contains(num)) {
+            int result = num * complement;
+            std::cout << "part 1: " << result << "\n";
+            break;
+        }
+        complements.insert(complement);
+    }
+}
+
+// Part 2 - Three sum
+void part2(std::vector<int>& nums) {
+    for (size_t i = 0; i < nums.size(); i++) {
+        int num1 = nums[i];
+
+        int remainingSum = 2020 - num1;
+
+        std::unordered_set<int> complements;
+        for (size_t j = i + 1; j < nums.size(); j++) {
+            int num2 = nums[j];
+            int complement = remainingSum - num2;
+            if (complements.contains(num2)) {
+                int result = num1 * num2 * complement;
+                std::cout << "part 2: " << result << "\n";
+                break;
+            }
+            complements.insert(complement);
+        }
+    }
+}
+
+std::vector<int> loadInput() {
+    std::vector<int> nums;
+    std::ifstream f{ "inputs\\day1.txt" };
+    int num;
+    while (f >> num) {
+        nums.push_back(num);
+    }
+    return nums;
+}
+
+int main() {
+    std::vector<int> nums = loadInput();
+    part1(nums);
+    part2(nums);
+    return 0;
+}
