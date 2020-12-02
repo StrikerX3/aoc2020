98cb6e420e1832dd2b9b0c98878927972bbb0087
StrikerX3 <ivan.rober@gmail.com>
2020-12-02 08:53:03 -0300

    Day 1: Display selected values

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -4,13 +4,13 @@
 #include <vector>
 
 // Part 1 - Two sum
-void part1(std::vector<int>& nums) {
+void part1(const std::vector<int>& nums) {
     std::unordered_set<int> complements;
     for (int num : nums) {
         int complement = 2020 - num;
         if (complements.contains(num)) {
             int result = num * complement;
-            std::cout << "part 1: " << result << "\n";
+            std::cout << "part 1: " << result << " (" << num << ", " << complement << ")\n";
             break;
         }
         complements.insert(complement);
@@ -18,7 +18,7 @@ void part1(std::vector<int>& nums) {
 }
 
 // Part 2 - Three sum
-void part2(std::vector<int>& nums) {
+void part2(const std::vector<int>& nums) {
     for (size_t i = 0; i < nums.size(); i++) {
         int num1 = nums[i];
 
@@ -30,7 +30,7 @@ void part2(std::vector<int>& nums) {
             int complement = remainingSum - num2;
             if (complements.contains(num2)) {
                 int result = num1 * num2 * complement;
-                std::cout << "part 2: " << result << "\n";
+                std::cout << "part 2: " << result << " (" << num1 << ", " << num2 << ", " << complement << ")\n";
                 break;
             }
             complements.insert(complement);
